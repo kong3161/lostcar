b493b8e4996b313246d8411f56dcad624071f352
kong3161 <socialism.voice@gmail.com>
2025-06-14 21:13:12 +0700

    ✅ อัปเดตโค้ดทั้งหมดล่าสุด

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ async def submit(
             import cloudinary.uploader
 
             cloudinary.config(
-                cloud_name="dibfdc1ft",
+                cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
                 api_key=os.getenv("CLOUDINARY_API_KEY"),
                 api_secret=os.getenv("CLOUDINARY_API_SECRET"),
                 secure=True
