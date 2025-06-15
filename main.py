cc2e8358fff7392d17f7ffe8a2a25e62058b50ea
kong3161 <socialism.voice@gmail.com>
2025-06-15 20:15:26 +0700

    ✅ อัปเดตโค้ดทั้งหมดล่าสุด

--- main.py
+++ main.py
@@ -266,7 +266,11 @@ from datetime import datetime
 async def show_map(request: Request, from_: str = None, to: str = None):
     supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-    query = supabase.table("reports").select("*").neq("lat", "0").neq("lng", "0")
+    query = supabase.table("reports") \
+        .select("*") \
+        .neq("lat", "0") \
+        .neq("lng", "0") \
+        .neq("date_lost", None)
 
     if from_ and to:
         query = query.gte("date_lost", from_).lte("date_lost", to)
